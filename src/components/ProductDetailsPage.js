e5ed00ca31ac34969b1c4a2657271f81fd7e70a9
Ben <benleviae5@gmail.com>
2025-01-26 17:15:42 +0200

    addToCart Bag Fixed

--- src/components/ProductDetailsPage.js
+++ src/components/ProductDetailsPage.js
@@ -1,11 +1,14 @@
 // src/components/ProductDetailsPage.js
-import React, { useState, useEffect } from "react"; // ייבוא של useState ו-useEffect
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux"; // ייבוא useDispatch
+import { addToCart } from "../redux/cartSlice"; // ייבוא addToCart
 
 const ProductDetailsPage = () => {
-  const { productId } = useParams(); // מקבלים את ה-Id של המוצר
+  const { productId } = useParams();
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch(); // הגדרת ה-dispatch
 
   useEffect(() => {
     // טוענים את המוצר על פי ה-Id שנשלח
@@ -29,6 +32,10 @@ const ProductDetailsPage = () => {
     return <p>Product not found.</p>;
   }
 
+  const handleAddToCart = () => {
+    dispatch(addToCart(product)); // שליחת המוצר לעגלת הקניות
+  };
+
   return (
     <div className="container my-4">
       <h1>{product.title}</h1>
@@ -42,11 +49,11 @@ const ProductDetailsPage = () => {
           />
         </div>
         <div className="col-md-6 mt-5">
-          {" "}
-          {/* שימוש ב-mt-4 להוספת רווח מעל */}
           <p className="pt-5 mt-5">{product.description}</p>
           <h3>${product.price}</h3>
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-primary" onClick={handleAddToCart}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
